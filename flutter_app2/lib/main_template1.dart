4be0bb068a6ab073986b471ed11ff380cbdbdd42
soiqualang <soiqualang.chentreu@gmail.com>
2019-05-15 15:49:33 +0700

    upload app2

---
+++ flutter_app2/lib/main_template1.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+      title: 'soiqualang',
+      home: new App1()
+  ));
+}
+
+class App1 extends StatelessWidget{
+   //Tao giao dien nguoi dung
+  @override
+  Widget build(BuildContext context) {
+
+
+    Container container = new Container(
+        padding: const EdgeInsets.all(16.0),
+        child: new Column(
+            children: [
+
+            ]
+        )
+    );
+
+    AppBar appBar = new AppBar(title: new Text("soiqualang"));
+
+    Scaffold scaffold = new Scaffold(appBar: appBar,
+        body: container);
+
+    return scaffold;
+
+  }
+}
