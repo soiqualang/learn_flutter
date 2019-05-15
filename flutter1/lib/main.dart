f030ab47288e975a1b917d502b69dc155b07850d
soiqualang <soiqualang.chentreu@gmail.com>
2019-05-15 11:56:51 +0700

    make tip app

--- flutter1/lib/main.dart
+++ flutter1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+//them ngon ngu
 import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
@@ -6,6 +7,12 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      home: RandomWords(),
+    );
+
+    /*
     //final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter 222',
@@ -15,21 +22,59 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
           //child: Text('Hello World 333'),
+          //them ngon ngu
           //child: Text(wordPair.asPascalCase),
           child: RandomWords(),
         ),
       ),
     );
+    */
   }
 }
 
 class RandomWordsState extends State<RandomWords> {
-  // TODO Add build() method
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider(); /*2*/
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+  /*
   @override
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
     return Text(wordPair.asPascalCase);
   }
+  */
 }
 
 class RandomWords extends StatefulWidget {
