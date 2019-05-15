7f3a0445ba6e0194bcc359bb22ba417de42aeceb
soiqualang <soiqualang.chentreu@gmail.com>
2019-05-15 16:11:59 +0700

    make text box

--- flutter_app2/lib/main.dart
+++ flutter_app2/lib/main.dart
@@ -8,16 +8,27 @@ void main() {
 }
 
 class App1 extends StatelessWidget{
+  String vtxt1;
    //Tao giao dien nguoi dung
   @override
   Widget build(BuildContext context) {
+    //Khi tao mot widget thi nho add no vao child cua container
+    TextField txt1=new TextField(
+      onChanged: (value){
+        try{
+          vtxt1=value;
+        }catch(exception){
+          vtxt1="";
+        }
+      },
+    );
 
 
     Container container = new Container(
         padding: const EdgeInsets.all(16.0),
         child: new Column(
             children: [
-
+              txt1
             ]
         )
     );
