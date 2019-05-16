a74051d06c7ce55343c33a091be2e909cc652810
soiqualang <soiqualang.chentreu@gmail.com>
2019-05-16 16:36:20 +0700

    finish

--- infinite_scrolling_listview/lib/main.dart
+++ infinite_scrolling_listview/lib/main.dart
@@ -1,44 +1,75 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
+// #docregion MyApp
 class MyApp extends StatelessWidget {
+  // #docregion build
   @override
   Widget build(BuildContext context) {
-
-    AppBar appBar = new AppBar(title: new Text("soiqualang"));
-    Center txt1=new Center(
-      //child: Text("hahahahaha",style: TextStyle(fontSize: 24),),
-      //child: Text(wordPair.asPascalCase),
-      child: RandomWords(),
-    );
-    Scaffold scaffold = new Scaffold(
-        appBar: appBar,
-        body: txt1,
-        drawer: Drawer(),
-    );
-
-
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: scaffold,
+      title: 'Startup Name Generator',
+      home: RandomWords(),
     );
   }
+// #enddocregion build
 }
+// #enddocregion MyApp
 
-class RandomWords extends StatefulWidget{
-  @override
-  RandomWordsState createState() => new RandomWordsState();
-}
+// #docregion RWS-var
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  // #enddocregion RWS-var
 
-class RandomWordsState extends State<RandomWords>{
+  // #docregion _buildSuggestions
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider(); /*2*/
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+  // #enddocregion _buildSuggestions
+
+  // #docregion _buildRow
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+  // #enddocregion _buildRow
+
+  // #docregion RWS-build
   @override
   Widget build(BuildContext context) {
-    final wordPair=WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
   }
+// #enddocregion RWS-build
+// #docregion RWS-var
+}
+// #enddocregion RWS-var
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
 }
