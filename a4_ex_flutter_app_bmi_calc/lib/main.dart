ab9b149cfb5e08b6a560ee6be48bb0123197eac2
soiqualang <soiqualang.chentreu@gmail.com>
2019-05-17 14:16:26 +0700

    finish v1

--- a4_ex_flutter_app_bmi_calc/lib/main.dart
+++ a4_ex_flutter_app_bmi_calc/lib/main.dart
@@ -19,14 +19,64 @@ class bmi_calc extends StatefulWidget{
 }
 
 class bmi_calc_State extends State<bmi_calc> {
+  double pheight=0;
+  double pweight=0;
+  double vbmi=0;
+  String txt_res="";
 
+  void bmi_calc(){
+    setState(() {
+      vbmi=pweight/(pheight*pheight);
+      txt_res="Your BMI: $vbmi";
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    //text height
+    TextField txt_height=new TextField(
+      decoration: InputDecoration(
+        labelText: "Your height:",
+      ),
+      keyboardType: TextInputType.numberWithOptions(),
+      onChanged: (v){
+        try{
+          pheight=double.parse(v);
+        }catch(exception){
+          pheight=0;
+        }
+      },
+    );
+    //text weight
+    TextField txt_weight=new TextField(
+      decoration: InputDecoration(
+        labelText: "Your weight:",
+      ),
+      keyboardType: TextInputType.numberWithOptions(),
+      onChanged: (v){
+        try{
+          pweight=double.parse(v);
+        }catch(exception){
+          pweight=0;
+        }
+      },
+    );
+    //text result
+    Text txt_kq=new Text(txt_res,style: TextStyle(fontSize: 24),);
+    //Button Calc
+    RaisedButton rbtn_calc=new RaisedButton(
+      child: Text("Calculator",style: TextStyle(fontSize: 18),),
+      onPressed: bmi_calc,
+    );
+
     Container container = new Container(
         padding: const EdgeInsets.all(16.0),
         child: new Column(
             children: [
+              txt_weight,
+              txt_height,
+              rbtn_calc,
+              txt_kq,
             ]
         )
     );
